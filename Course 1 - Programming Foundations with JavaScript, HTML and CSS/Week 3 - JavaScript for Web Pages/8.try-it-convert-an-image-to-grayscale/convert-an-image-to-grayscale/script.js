e786a2854e2d042d47afe06c2d7299a76f6f272c
Ginny-Dang <ginnydang0728@gmail.com>
2022-07-03 11:18:39 -0500

    Update Week 3 of Course 1

--- Course 1 - Programming Foundations with JavaScript, HTML and CSS/Week 3 - JavaScript for Web Pages/8.try-it-convert-an-image-to-grayscale/convert-an-image-to-grayscale/script.js
+++ Course 1 - Programming Foundations with JavaScript, HTML and CSS/Week 3 - JavaScript for Web Pages/8.try-it-convert-an-image-to-grayscale/convert-an-image-to-grayscale/script.js
@@ -1,23 +1,32 @@
-var imgcanvas = document.getElementById("can");
+var imgcanvas = document.getElementById("can1");
 var fileinput = document.getElementById("finput");
-var image;
+var grayscalecanvas = document.getElementById("can2");
+// var image;
+var originalimage;
+var grayscaleimage;
 
 function upload() {
-    image = new SimpleImage(fileinput);
-    image.drawTo(imgcanvas);
-    
+    //Get the input image
     var filename = fileinput.value;
     alert("You choose " + filename);
+
+    // Draw the input image to both canvas
+    originalimage = new SimpleImage(fileinput);
+    originalimage.drawTo(imgcanvas);
+    
+    grayscaleimage = new SimpleImage(fileinput);
+    grayscaleimage.drawTo(grayscalecanvas);
+    
 }
 
 function makeGray() {
-    for (var pixel of image.values()) {
+    for (var pixel of originalimage.values()) {
         var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3
         pixel.setRed(avg);
         pixel.setGreen(avg);
         pixel.setBlue(avg);
     }
 
-    var imgcanvas = document.getElementById("can");
-    image.drawTo(imgcanvas);
+    var grayscalecanvas = document.getElementById("can2");
+    originalimage.drawTo(grayscalecanvas);
 }
