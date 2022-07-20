58a55a0e9a41e9fc5ebcc2ae077838526b320b3a
Ginny-Dang <ginnydang0728@gmail.com>
2022-07-19 22:50:08 -0500

    Style Course 5 project user interface

--- Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/RecommendationRunner.java
+++ Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/RecommendationRunner.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class RecommendationRunner implements Recommender {
+    @Override
     public ArrayList<String> getItemsToRate() {
         ArrayList<String> itemIds = new ArrayList<String>();
         // Get movies released during or after 2010
@@ -8,12 +9,18 @@ public class RecommendationRunner implements Recommender {
         YearAfterFilter yearAfterFilter = new YearAfterFilter(year);
         ArrayList<String> movies = MovieDatabase.filterBy(yearAfterFilter);
         // Only choose 20 items
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
+            Random rand = new Random();
+            int random = rand.nextInt(movies.size());
+            while (itemIds.contains(movies.get(random))) {
+                random = rand.nextInt(movies.size());
+            }
             itemIds.add(movies.get(i));
         }
         return itemIds;
     }
     
+    @Override
     public void printRecommendationsFor(String webRaterID) {
         // Initialize
         String moviefile = "ratedmoviesfull.csv";
@@ -25,33 +32,113 @@ public class RecommendationRunner implements Recommender {
         rater.initialize(ratingsfile);
         
         // Get recommendations
-        int numSimilarRaters = 10;
+        int numSimilarRaters = 20;
         int minimalRaters = 5;
-        AllFilters filters = new AllFilters();
-        filters.addFilter(new GenreFilter("Romance"));
-        filters.addFilter(new MinutesFilter(80, 160));
-        ArrayList<Rating> allRecommendations = fourthRatings.getSimilarRatingsByFilter(webRaterID, numSimilarRaters, minimalRaters, filters);
-        ArrayList<Rating> topRecommendations = new ArrayList<Rating>();
-        for (int i = 0; i < 10; i++) {
-            if (i == allRecommendations.size()) {
-                break;
-            }
-            topRecommendations.add(allRecommendations.get(i));
-        }
-        
-        if (topRecommendations.isEmpty()) {
-            System.out.println("I'm sorry, no recommendations found");
+        ArrayList<Rating> recommendations = fourthRatings.getSimilarRatings(webRaterID, numSimilarRaters, minimalRaters);
+        if (recommendations.size() == 0) {
+            System.out.println("<h2>Sorry, there is no movie recommend for you based on your rating!</h2>");
         } else {
-            for (int i = 0; i < topRecommendations.size(); i++) {
-                Rating item = topRecommendations.get(i);
-                String itemId = item.getItem();
-                String title = movie.getTitle(itemId);
-                String genres = movie.getGenres(itemId);
-                String directors = movie.getDirector(itemId);
-                System.out.println(i + 1 + ". " + title + " | ");
-                System.out.println("\tGenre(s): " + genres + " | ");
-                System.out.println("\tDirector(s): " + directors);
+            ArrayList<String> movieToBeRated = getItemsToRate();
+            ArrayList<Rating> outID = new ArrayList<>();
+            int count = 0;
+            for (int i = 0; outID.size() + count != recommendations.size() && outID.size() < 10; i++) {
+                if (!movieToBeRated.contains(recommendations.get(i).getItem())) {
+                    outID.add(recommendations.get(i));
+                } else {
+                    count++;
+                }
+            }
+            
+            System.out.println("<style>");
+            System.out.println("h2, h3 {");
+            System.out.println("  text-align: center;");
+            System.out.println("  height: 50px;");
+            System.out.println("  line-height: 50px;");
+            System.out.println("  font-family: Arial, Helvetica, sans-serif;");
+            System.out.println("  background-color: #E0E0E0;");
+            System.out.println("  color: #001A57;");
+            System.out.println("}");
+            
+            System.out.println("table {");
+            System.out.println("  border-collapse: collapse;");
+            System.out.println("  margin: auto;");
+            System.out.println("}");
+            
+            System.out.println("table, th, td {");
+            System.out.println("  border: 2px solid white;");
+            System.out.println("  font-size: 15px;");
+            System.out.println("  padding: 2px 6px 2px 6px;");
+            System.out.println("}");
+            
+            System.out.println("td img {");
+            System.out.println("  display: block;");
+            System.out.println("  margin-left: auto;");
+            System.out.println("  margin-right: auto;");
+            System.out.println("}");
+            
+            System.out.println("th {");
+            System.out.println("  height: 40px;");
+            System.out.println("  font-size: 18px;");
+            System.out.println("  background-color: #E0E0E0;");
+            System.out.println("  color: #001A57;");
+            System.out.println("  text-align: center;");
+            System.out.println("}");
+            
+            System.out.println("tr:nth-child(even) {");
+            System.out.println("  background-color: #C4DDFF;");
+            System.out.println("}");
+            
+            System.out.println("tr:nth-child(odd) {");
+            System.out.println("  background-color: #7FB5FF;");
+            System.out.println("}");
+            
+            System.out.println("tr:hover {");
+            System.out.println("  background-color: #001D6E;");
+            System.out.println("  color:white;");
+            System.out.println("}");
+            
+            System.out.println("table td:first-child {");
+            System.out.println("  text-align: center;");
+            System.out.println("}");
+            
+            System.out.println("tr {");
+            System.out.println("  font-family: Arial, Helvetica, sans-serif;");
+            System.out.println("}");
+            
+            System.out.println(".rating {");
+            System.out.println("  color:#ff6600;");
+            System.out.println("  padding: 0px 10px;");
+            System.out.println("  font-weight: bold;");
+            System.out.println("}");
+            System.out.println("</style>");
+            
+            System.out.println("<h2>Movie Recommendations</h2>");
+            System.out.println("<table id = \"rater\">");
+            System.out.println("<tr>");
+            System.out.println("<th>Rank</th>");
+            System.out.println("<th>Poster</th>");
+            System.out.println("<th>Title & Rating</th>");
+            System.out.println("<th>Genre</th>");
+            System.out.println("<th>Director</th>");
+            System.out.println("<th>Country</th>");
+            System.out.println("</tr>");
+            
+            int rank = 1;
+            for (Rating r : outID) {
+                String id = r.getItem();
+                System.out.println("<tr><td>" + rank + "</td>" +
+                        "<td><img src = \"" + MovieDatabase.getPoster(id) + "\" width=\"50\" height=\"70\"></td>" +
+                        "<td>" + MovieDatabase.getYear(id) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" +
+                        id + "\">" + MovieDatabase.getTitle(id) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;"
+                        + String.format("%.1f", r.getValue()) + "/10</td>" +
+                        "<td>" + MovieDatabase.getGenres(id) + "</td>" +
+                        "<td>" + MovieDatabase.getDirector(id) + "</td>" +
+                        "<td>" + MovieDatabase.getCountry(id) + "</td>" +
+                        "</tr>");
+                rank += 1;
             }
         }
+        System.out.println("</table>");
+        System.out.println("<h3>*The rank of movies is based on other raters who have the most similar rating to yours.</h3>");
     }
 }
