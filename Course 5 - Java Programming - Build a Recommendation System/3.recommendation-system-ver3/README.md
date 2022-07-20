1bc907ce56235c7d602b45ff50c4bb6adeca784e
Ginny-Dang <ginnydang0728@gmail.com>
2022-07-20 16:26:13 -0500

    Update project README.md file and remove unnecessary code

--- Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/README.md
+++ Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/README.md
@@ -38,4 +38,60 @@ Specifically for this assignment you will do the following:
   - Write a void method printSimilarRatingsByGenreAndMinutes that has no parameters. This method is similar to printSimilarRatings but also uses a genre filter and a minutes filter and then lists recommended movies and their similarity ratings, and for each movie prints the movie, its minutes, and its similarity rating on one line and its genres on a separate line below it. For example, using the files ratedmoviesfull.csv and ratings.csv, the genre “Adventure”,  minutes between 100 and 200 inclusive, the rater ID 65, the number of minimal raters set to 5, and the number of top similar raters set to 10, the movie returned with the top rated average is “Interstellar”.
   - Write a void method printSimilarRatingsByYearAfterAndMinutes that has no parameters. This method is similar to printSimilarRatings but also uses a year-after filter and a minutes filter and then lists recommended movies and their similarity ratings, and for each movie prints the movie, its year, its minutes, and its similarity rating on one line. For example, using the files ratedmoviesfull.csv and ratings.csv, the year 2000, minutes between 80 and 100 inclusive, the rater ID 65, the number of minimal raters set to 5, and the number of top similar raters set to 10, the movie returned with the top rated average is “The Grand Budapest Hotel”.
 
-### _(This version of the recommendation system is more effecient than the second one as it has the ratings database optimized with a HashMap. Also, it was completed by finding users in the database that have similar ratings and weighting their input to provide a more personal recommendation for the users of the program)_
+# Programming Exercise: Step Five
+
+For the last part of the capstone, you will integrate your recommender program with our course site. This will enable you to let a user run your program interactively on the internet. The user will be presented with a list of movies to rate, they will submit their ratings, and then a list of movies recommended for them by your program will be displayed. You will be able to choose which movies the user is presented to rate, and how to display the recommended movies.
+
+You will have to write a class that will allow the course site to run your recommender program, and then you will have to create a zip file of the code to be uploaded.
+
+## Write a Class
+
+For this assignment you will be given one interface __Recommender__. You will have to write a class __RecommendationRunner__ that implements __Recommender__. The two methods you will need to implement are:
+- getItemsToRate()
+- printRecommendationsFor()
+
+When the user first visits the recommender site, our code will call the method __getItemsToRate()__ to get a list of movies to display on the web page for users to rate. After the user submits their ratings, our code will call the method __printRecommendationsFor()__ to get your recommendations based on the user's ratings and display them.
+
+Specifically, you should:
+- Create a new class named __RecommendationRunner__ that implements __Recommender__.
+- Write the method __getItemsToRate()__. It returns a list of strings representing movie IDs that will be used to present movies to the user for them to rate. You can choose how to select movies for this list, for example, you could select recent movies, movies from a specific genre, randomly chosen movies, or something else. The movies returned by this method will be displayed on a web page, so the number of movies you choose to return may affect how long the page takes to load, and how willing users will be to rate the movies. 10-20 movies should be fine to get a good profile of the user’s opinions, but 50 may be too many.
+- Write the void method __printRecommendationsFor()__. It prints out an HTML table of movies recommended by your program for the user based on the movies they rated. It has one parameter __webRaterID__, a String that is the ID of the user, who has been added by our code to the __RaterDatabase__ with the ratings they entered. To get the movies recommended by your program, you may want to use your __FourthRatings__ class. Because the HTML printed by this method will be displayed on a webpage, the number of recommended movies you choose to display may affect how long the page takes to load. For example, you may want to display only the top 10-20 recommended movies, or to not include movies the user rated. In some rare cases there may not be any recommended movies, for example, if no movies were rated by the number of minimal raters specified in the recommender. If no movies are recommended, you should notify the user with a message. Whatever is printed by this method will be displayed on the web page: HTML, plain text, or debugging information. If you want to style this HTML page, please include the CSS styling directly within the page using the `<style>` tag.
+
+## Create the zip file
+
+To run your program on our course site, you will upload a single zip file containing some of your Java class files. You will need to include:
+- __FourthRatings__
+- __RecommendationRunner__
+- Any other supporting classes you wrote and want to use (for example, if you use any of the filters you wrote, you will need to include them here).
+
+Note that you only need to include class files, not the original java files, so all files you include should have a .class extension. You do not need to include files we gave you such as __MovieDatabase__. Here is an example of the contents of the zip file:
+
+<img src="./pic1.png" width=250>
+
+In this case, the learner has chosen to include their __TrueFilter__. You may wish to include other filters you wrote.
+
+To create a zip file on a Mac, select the files you would like to include in the zip file while holding the command key. Once you have selected all the files you would like to include, release the command key, and right-click on one of the selected items. You should see something like this:
+
+<img src="./pic2.png" width=400>
+
+Select ‘Compress n Items’ (n will be the number of items you have selected). You should now see a file Archive.zip in the same folder:
+
+<img src="./pic3.png" width=400>
+
+You can rename this file if you would like, but make sure it still has a .zip extension.
+
+To create a zip file on a Windows machine, select the files you would like to include in the zip file while holding the control key. Once you have selected all the files you would like to include, release the control key, and right-click on one of the selected items. You should see something like this:
+
+<img src="./pic4.png" width=500>
+
+Select ‘Send to’ then ‘Compressed (zipped) folder’. You should now see a zip file in the same folder:
+
+<img src="./pic5.png" width=500>
+
+It has the same name as whichever file you selected last (in this example, TrueFilter). You can rename this file if you would like.
+
+## Run Your Code Online
+
+To run your code online, upload your zip file at http://www.dukelearntoprogram.com/capstone/upload.php and press ‘Submit Code’. If there were no problems with your file, you should see a page with a link to click to run your program interactively. Clicking this link calls the __getItemsToRate()__ method, so you will see a page with movies to rate. To share your program with others, copy the URL of this page with the list of movies to rate (not the upload page) and send it to whoever you would like to share your program with.
+
+### _(This version of the recommendation system is more effecient than the second one as it has the ratings database optimized with a HashMap. Also, it was completed by finding users in the database that have similar ratings and weighting their input to provide a more personal recommendation for the users of the program. Besides, this version has a style User Interface.)_
--- Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/RecommendationRunner.java
+++ Course 5 - Java Programming - Build a Recommendation System/3.recommendation-system-ver3/RecommendationRunner.java
@@ -20,6 +20,131 @@ public class RecommendationRunner implements Recommender {
         return itemIds;
     }
     
+    /*
+    @Override
+    public void printRecommendationsFor(String webRaterID) {
+        // Initialize
+        String moviefile = "ratedmoviesfull.csv";
+        String ratingsfile = "ratings.csv";
+        FourthRatings fourthRatings = new FourthRatings();
+        MovieDatabase movie = new MovieDatabase();
+        RaterDatabase rater = new RaterDatabase();
+        movie.initialize(moviefile);
+        rater.initialize(ratingsfile);
+        int numSimilarRaters = 20;
+        int minimalRaters = 5;
+        
+        try{
+            ArrayList<Rating> recommendations = fourthRatings.getSimilarRatings(webRaterID, numSimilarRaters, minimalRaters);
+            System.out.println("My weRaterID is " + webRaterID);
+            System.out.println("recommendations was build?");
+            System.out.println(recommendations.size());
+            
+            if (recommendations.size() < 10) {
+                System.out.println("<h2>Sorry, there is no movie recommend for you based on your rating!</h2>");
+            } else {
+                ArrayList<Rating> outID = new ArrayList<>();
+                for (int n = 0; n < 10; n++) {
+                    outID.add(recommendations.get(n));
+                }
+            
+                System.out.println("<style>");
+                System.out.println("h2, h3 {");
+                System.out.println("  text-align: center;");
+                System.out.println("  height: 50px;");
+                System.out.println("  line-height: 50px;");
+                System.out.println("  font-family: Arial, Helvetica, sans-serif;");
+                System.out.println("  background-color: #E0E0E0;");
+                System.out.println("  color: #001A57;");
+                System.out.println("}");
+            
+                System.out.println("table {");
+                System.out.println("  border-collapse: collapse;");
+                System.out.println("  margin: auto;");
+                System.out.println("}");
+            
+                System.out.println("table, th, td {");
+                System.out.println("  border: 2px solid white;");
+                System.out.println("  font-size: 15px;");
+                System.out.println("  padding: 2px 6px 2px 6px;");
+                System.out.println("}");
+                
+                System.out.println("td img {");
+                System.out.println("  display: block;");
+                System.out.println("  margin-left: auto;");
+                System.out.println("  margin-right: auto;");
+                System.out.println("}");
+            
+                System.out.println("th {");
+                System.out.println("  height: 40px;");
+                System.out.println("  font-size: 18px;");
+                System.out.println("  background-color: #E0E0E0;");
+                System.out.println("  color: #001A57;");
+                System.out.println("  text-align: center;");
+                System.out.println("}");
+            
+                System.out.println("tr:nth-child(even) {");
+                System.out.println("  background-color: #C4DDFF;");
+                System.out.println("}");
+            
+                System.out.println("tr:nth-child(odd) {");
+                System.out.println("  background-color: #7FB5FF;");
+                System.out.println("}");
+            
+                System.out.println("tr:hover {");
+                System.out.println("  background-color: #205375;");
+                System.out.println("  color:white;");
+                System.out.println("}");
+            
+                System.out.println("table td:first-child {");
+                System.out.println("  text-align: center;");
+                System.out.println("}");
+            
+                System.out.println("tr {");
+                System.out.println("  font-family: Arial, Helvetica, sans-serif;");
+                System.out.println("}");
+            
+                System.out.println(".rating {");
+                System.out.println("  color:#ff6600;");
+                System.out.println("  padding: 0px 10px;");
+                System.out.println("  font-weight: bold;");
+                System.out.println("}");
+                System.out.println("</style>");
+            
+                System.out.println("<h2>Movie Recommendations</h2>");
+                System.out.println("<table id = \"rater\">");
+                System.out.println("<tr>");
+                System.out.println("<th>Rank</th>");
+                System.out.println("<th>Poster</th>");
+                System.out.println("<th>Title & Rating</th>");
+                System.out.println("<th>Genre</th>");
+                System.out.println("<th>Director</th>");
+                System.out.println("<th>Country</th>");
+                System.out.println("</tr>");
+            
+                int rank = 1;
+                for (Rating r : outID) {
+                    String id = r.getItem();
+                    System.out.println("<tr><td>" + rank + "</td>" +
+                        "<td><img src = \"" + MovieDatabase.getPoster(id) + "\" width=\"50\" height=\"70\"></td>" +
+                        "<td>" + MovieDatabase.getYear(id) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" + id + "\">" 
+                        + MovieDatabase.getTitle(id) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;" 
+                        + String.format("%.1f", r.getValue()) + "/10</td>" +
+                        "<td>" + MovieDatabase.getGenres(id) + "</td>" +
+                        "<td>" + MovieDatabase.getDirector(id) + "</td>" +
+                        "<td>" + MovieDatabase.getCountry(id) + "</td>" +
+                        "</tr>");
+                    rank += 1;
+                }
+            }
+            System.out.println("</table>");
+            System.out.println("<h3>*The rank of movies is based on other raters who have the most similar rating to yours.</h3>");
+        } catch(Exception e) {
+            System.out.println("I'm sorry, there's a systme error. Please go back and try again");
+        }
+    }
+    */
+    
     @Override
     public void printRecommendationsFor(String webRaterID) {
         // Initialize
@@ -35,18 +160,17 @@ public class RecommendationRunner implements Recommender {
         int numSimilarRaters = 20;
         int minimalRaters = 5;
         ArrayList<Rating> recommendations = fourthRatings.getSimilarRatings(webRaterID, numSimilarRaters, minimalRaters);
-        if (recommendations.size() == 0) {
+        // debug
+        System.out.println("My weRaterID is " + webRaterID);
+        System.out.println("recommendations was build?");
+        System.out.println(recommendations.size());
+        
+        if (recommendations.size() < 10) {
             System.out.println("<h2>Sorry, there is no movie recommend for you based on your rating!</h2>");
         } else {
-            ArrayList<String> movieToBeRated = getItemsToRate();
             ArrayList<Rating> outID = new ArrayList<>();
-            int count = 0;
-            for (int i = 0; outID.size() + count != recommendations.size() && outID.size() < 10; i++) {
-                if (!movieToBeRated.contains(recommendations.get(i).getItem())) {
-                    outID.add(recommendations.get(i));
-                } else {
-                    count++;
-                }
+            for (int n = 0; n < 10; n++) {
+                outID.add(recommendations.get(n));
             }
             
             System.out.println("<style>");
@@ -93,7 +217,7 @@ public class RecommendationRunner implements Recommender {
             System.out.println("}");
             
             System.out.println("tr:hover {");
-            System.out.println("  background-color: #001D6E;");
+            System.out.println("  background-color: #205375;");
             System.out.println("  color:white;");
             System.out.println("}");
             
@@ -128,8 +252,8 @@ public class RecommendationRunner implements Recommender {
                 String id = r.getItem();
                 System.out.println("<tr><td>" + rank + "</td>" +
                         "<td><img src = \"" + MovieDatabase.getPoster(id) + "\" width=\"50\" height=\"70\"></td>" +
-                        "<td>" + MovieDatabase.getYear(id) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" +
-                        id + "\">" + MovieDatabase.getTitle(id) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;"
+                        "<td>" + MovieDatabase.getYear(id) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" + id + "\">" 
+                        + MovieDatabase.getTitle(id) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;" 
                         + String.format("%.1f", r.getValue()) + "/10</td>" +
                         "<td>" + MovieDatabase.getGenres(id) + "</td>" +
                         "<td>" + MovieDatabase.getDirector(id) + "</td>" +
